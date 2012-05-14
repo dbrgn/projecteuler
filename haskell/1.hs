3007f40a25b68ce180a06e9fe29181a668dbc3b0
Danilo Bargen <gezuru@gmail.com>
2012-05-15 01:14:43 +0200

    Initial version of problem #1 in Haskell

---
+++ haskell/1.hs
@@ -0,0 +1,16 @@
+-- PROBLEM 1:
+--
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+-- get 3, 5, 6 and 9. The sum of these multiples is 23.
+--
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+isvalid :: Int -> Bool
+isvalid x | mod x 3 == 0  = True
+          | mod x 5 == 0  = True
+          | otherwise     = False
+
+main = do
+    let srclist = [1..999]
+    let valid_numbers = filter isvalid srclist
+    print $ sum valid_numbers
