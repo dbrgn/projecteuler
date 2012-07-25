8f7358312d546e525709d9338bff9455a3bee370
Danilo Bargen <gezuru@gmail.com>
2012-07-26 00:06:15 +0200

    Solution to #10

--- python/3.py
+++ python/3.py
@@ -1,53 +1,11 @@
+# BROKEN
 import sys
+from primes import getprimes
 
-
-def getprimes(): 
-    """
-    Generate an infinite sequence of prime numbers.
-    """
-    # Maps composites to primes witnessing their compositeness.
-    # This is memory efficient, as the sieve is not "run forward"
-    # indefinitely, but only as long as required by the current
-    # number being tested.
-    #
-    D = {}  
-
-    # The running integer that's checked for primeness
-    q = 2  
-
-    while True:
-        if q not in D:
-            # q is a new prime.
-            # Yield it and mark its first multiple that isn't
-            # already marked in previous iterations
-            # 
-            yield q        
-            D[q * q] = [q]
-        else:
-            # q is composite. D[q] is the list of primes that
-            # divide it. Since we've reached q, we no longer
-            # need it in the map, but we'll mark the next 
-            # multiples of its witnesses to prepare for larger
-            # numbers
-            # 
-            for p in D[q]:
-                D.setdefault(p + q, []).append(p)
-            del D[q]
-
-        q += 1
-
-    
-
-
-number = int(sys.argv[1])
 factors = []
-
-n = number
-for prime in getprimes():
-    if prime > n:
-        break
+n = int(sys.argv[1])
+for prime in getprimes(int(sys.argv[1])):
     if not n % prime:
         factors.append(prime)
         n = n / prime
-
 print factors
