a4b4e0cb58c2e6cdf7723dc0f81497f1d50e6025
Danilo Bargen <gezuru@gmail.com>
2013-03-18 11:04:06 +0100

    Sieve of Eratosthenes (3.sql)

--- plsql/3.sql
+++ plsql/3.sql
@@ -1,26 +1,36 @@
 -- WORK IN PROGRESS, NOT YET FINISHED!
 
-create or replace procedure erastothenes as
+CREATE OR REPLACE PROCEDURE Eratosthenes AS
   primes dbms_sql.number_table;
-  max_number number := 120;
-  next_potential_prime number := 2;
-begin
+  max_number NUMBER := 120;
+  next_potential_prime NUMBER := 2;
+  loop_tmp NUMBER;
+  loop_step NUMBER;
+BEGIN
 
   -- initialize table
-  for i in 2..max_number loop
+  FOR i IN 2..max_number LOOP
     primes(i) := 1;
-  end loop;
+  END LOOP;
 
-  -- main loop
-  for i in 2..max_number loop
-    if primes(i) = 1 then
-      null;
-      -- TODO continue here
-    end if;
-  end loop;
+  -- main loop, mark non-primes with 0
+  FOR i IN 2..sqrt(max_number) LOOP
+    IF primes(i) = 1 THEN
+      loop_tmp := i * i;
+      loop_step := 1;
+      WHILE loop_tmp <= max_number LOOP
+        primes(loop_tmp) := 0;
+        loop_tmp := loop_tmp + loop_step * i;
+      END LOOP;
+    END IF;
+  END LOOP;
 
-  for i in 2..max_number loop
-    dbms_output.put_line('Prime ' || i || ': ' || primes(i));
-  end loop;
+  -- output result
+  FOR i IN 2..max_number LOOP
+    IF primes(i) = 1 THEN
+      dbms_output.put(i || ' ');
+    END IF;
+  END LOOP;
+  dbms_output.put_line('');
 
-end erastothenes;
+END Eratosthenes;
