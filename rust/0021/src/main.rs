322b2220185b6b20a273eb5c3a87833d1afb318b
Danilo Bargen <mail@dbrgn.ch>
2015-04-16 18:13:17 +0200

    [rust/21] Nicer solution, still room for performance optimization

--- rust/0021/src/main.rs
+++ rust/0021/src/main.rs
@@ -11,44 +11,30 @@
 //!
 //! Evaluate the sum of all the amicable numbers under 10000.
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
+
+fn divisor_sum(number: u32) -> u32 {
+    (1..number/2+1)
+        .filter(|&x| number % x == 0)
+        .fold(0, |a, b| a + b)
+}
 
 fn main() {
-    let max = 10000;
-
-    let mut solutions: HashMap<u32, u32> = HashMap::new();
+    let max = 10_000;
     let mut amicable_numbers: HashSet<u32> = HashSet::new();
 
-    // Calculate prober divisor sum for all numbers
-    for n in (2..max*2) {  // I feel bad for that * 2...
-        let dn = (1..n/2+1).filter(|&x| n % x == 0).fold(0, |a, b| a + b);
-        solutions.insert(n, dn);
-        println!("Solution pair: d({}) = {}", n, dn);
-    }
-
-    // Find amicable pairs
-    for (n, dn) in solutions.iter() {
-        if n != dn {  // Ignore equal pairs
-            match solutions.get(dn) {
-                Some(reverse_n) => {
-                    if n == reverse_n && n <= &max {
-                        amicable_numbers.insert(*n);
-                        amicable_numbers.insert(*dn);
-                    }
-                },
-                None => (),
-            }
+    // Find and print amicable numbers
+    for n in (1..max) {
+        let dn = divisor_sum(n);
+        if n != dn && divisor_sum(dn) == n {
+            println!("Amicable pair: {} <=> {}", n, dn);
+            amicable_numbers.insert(n);
+            amicable_numbers.insert(dn);
         }
-    }
-
-    // Print amicable numbers
-    for num in &amicable_numbers {
-        println!("Amicable number: {}", num);
     }
 
     // Print sum
     let amicable_sum = amicable_numbers.iter().fold(0, |a, b| a + *b);
-    println!("Sum of amicable numbers <= {}: {}", max, amicable_sum);
-
+    println!("Sum of amicable numbers: {}", amicable_sum);
 }
